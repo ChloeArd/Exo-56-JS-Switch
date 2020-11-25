bfddbb22b8adc2d75bdb90a29861801819a97e06
ChloeArd <chloe.ardoise@gmail.com>
2020-11-25 14:18:40 +0100

    Exercice 56

---
+++ app.js
@@ -0,0 +1,24 @@
+let accessoire = prompt("Entrez un nom d'un accessoire informatique EN MINUSCULE");
+
+switch (accessoire) {
+    case "souris":
+        alert("J'utilise une " + accessoire);
+        break;
+    case "clavier":
+        alert("J'utilise un " + accessoire);
+        break;
+    case "ordinateur":
+        alert("J'utilise un " + accessoire);
+        break;
+    case "tapis":
+        alert("J'utilise un " + accessoire);
+        break;
+    case "imprimante":
+        alert("J'utilise une " + accessoire);
+        break;
+    case "ondulateur":
+        alert("J'utilise un " + accessoire);
+        break;
+    default:
+        alert("L'accessoire est inconnu !");
+}
